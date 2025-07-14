1af535d57c7d8af49f200bafda71721f2a8a08c7
kevinsultana <kevinpdg.ks@gmail.com>
2025-07-14 23:23:44 +0800

    feat: initial main and auth layout structure

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+import AuthLayout from "../layout/AuthLayout";
+import Home from "../pages/Home";
+import LoginPage from "../pages/Login";
+import RegisterPage from "../pages/Register";
 
-export default function router() {
-  return <div>router</div>;
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "register",
+        element: <RegisterPage />,
+      },
+    ],
+  },
+]);
+
+export default router;

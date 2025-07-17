284baa71722fef919891e49c5ecf3c4fbca6d5af
kevinsultana <kevinpdg.ks@gmail.com>
2025-07-17 23:03:57 +0800

    feat: initial function login page

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,22 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAj7hpDY7LN7LHwQTJPjpvgf_gJnu-trm0",
-  authDomain: "p2-gc01.firebaseapp.com",
-  projectId: "p2-gc01",
-  storageBucket: "p2-gc01.firebasestorage.app",
-  messagingSenderId: "692085629125",
-  appId: "1:692085629125:web:9548ae1b6dcd2373ccd0ac",
-  measurementId: "G-E5HCZJMFPW",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
-const analytics = getAnalytics(app);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,48 @@
-import React from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { use, useState } from "react";
+import { auth } from "../firebase/firebase";
+import { useNavigate } from "react-router";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    // console.log(email, password);
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const navigate = useNavigate();
+
+  const handleRegister = () => {
+    navigate("/auth/register");
+  };
+
+  return (
+    <div>
+      <div>Login</div>
+      <form className="flex flex-col">
+        <label>Email</label>
+        <input
+          type="text"
+          placeholder="Enter your email..."
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>password</label>
+        <input
+          type="text"
+          placeholder="Enter your password..."
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={(e) => handleLogin(e)}>Login</button>
+      </form>
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
 }

fee0c7228b3b6b0466a5ffb6d3265dce23c2802f
kevinsultana <kevinpdg.ks@gmail.com>
2025-07-20 17:47:09 +0700

    feat: add confirmation logout modal and style home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,21 @@ import { useNavigate } from "react-router";
 
 export default function Navbar() {
   const [showAccMenu, setShowAccMenu] = useState(false);
+  const [modalConfirmation, setModalConfirmation] = useState(false);
+  const [animateModal, setAnimateModal] = useState(false);
 
   const navigate = useNavigate();
+
+  const openModal = () => {
+    setModalConfirmation(true);
+    setShowAccMenu(false);
+    setTimeout(() => setAnimateModal(true), 10);
+  };
+
+  const closeModal = () => {
+    setAnimateModal(false);
+    setTimeout(() => setModalConfirmation(false), 300);
+  };
 
   const handleLogOut = () => {
     auth.signOut().then(() => {
@@ -23,7 +36,10 @@ export default function Navbar() {
       </div>
       <div className="flex gap-4 text-black dark:text-white">
         <FaCartShopping />
-        <FaUser onClick={() => setShowAccMenu(!showAccMenu)} />
+        <FaUser
+          className="cursor-pointer"
+          onClick={() => setShowAccMenu(!showAccMenu)}
+        />
       </div>
 
       {showAccMenu && (
@@ -33,12 +49,41 @@ export default function Navbar() {
               Profile
             </li>
             <li
-              onClick={handleLogOut}
+              onClick={() => {
+                openModal();
+              }}
               className="cursor-pointer text-black dark:text-white hover:text-blue-400  transition-all duration-300"
             >
               Logout
             </li>
           </ul>
+        </div>
+      )}
+      {modalConfirmation && (
+        <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+          <div
+            className={`bg-white dark:bg-gray-800 rounded-lg p-6 w-11/12 max-w-lg relative transform transition-all duration-300 ${
+              animateModal ? "scale-100 opacity-100" : "scale-90 opacity-0"
+            }`}
+          >
+            <p className="text-black dark:text-white">
+              Are you sure you want to logout?
+            </p>
+            <div className="flex justify-between mt-4">
+              <button
+                onClick={() => closeModal()}
+                className="mr-2 px-4 py-2 bg-gray-200 dark:bg-gray-700 text-black dark:text-white rounded-lg hover:bg-gray-300 dark:hover:bg-gray-500 transition-all duration-300"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleLogOut}
+                className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-all duration-300"
+              >
+                Logout
+              </button>
+            </div>
+          </div>
         </div>
       )}
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,12 @@ export default function Home() {
   const { user } = useContext(AuthContext);
 
   return (
-    <div className="bg-white dark:bg-gray-800 text-black dark:text-white transition-all duration-300">
-      <div>Home Page</div>
-      <h1>hello {user?.email}</h1>
+    <div className=" bg-white dark:bg-gray-800 text-black dark:text-white transition-all duration-300">
+      <div className="flex flex-col items-center justify-center h-96 ">
+        <div>Home Page</div>
+        <p>hello {user?.email}</p>
+        <h1 className="animate-pulse text-7xl my-4">Under Development</h1>
+      </div>
     </div>
   );
 }
